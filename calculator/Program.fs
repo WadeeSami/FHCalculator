d99143e6c8188061b0f016c002cf415f3ea5d2a6
Wadee Sami <wadee.abuzant@live.com>
2019-03-30 08:42:38 +0300

    STEP 1: handle empty, single and 2 digits.

--- calculator/Program.fs
+++ calculator/Program.fs
@@ -1,1 +1,23 @@
-﻿
+﻿open System
+open System.Text.RegularExpressions
+
+let (|SingleDigitString|_|) input =
+   let m = Regex.Match(input, @"^[0-9]+$")
+   if (m.Success) then Some input else None
+
+let (|TwoDigitString|_|) input =
+   let m = Regex.Match(input, @"^[0-9]+,[0-9]+$")
+   if (m.Success) then Some input else None
+
+let rec add s =
+    match s with
+    | "" ->
+        0
+    | SingleDigitString x ->
+        int x
+    | TwoDigitString x ->
+        x.Split ',' |> Seq.map int |> Seq.sum
+    | _ ->
+        failwith "message"
+
+printfn "%i" (add "76,3456")
