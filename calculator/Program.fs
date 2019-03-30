8bbc57e299695d6287dc6ccbad21e308d79a7f36
Wadee Sami <wadee.abuzant@live.com>
2019-03-30 13:28:58 +0300

    STEP4: support any delimiter

--- calculator/Program.fs
+++ calculator/Program.fs
@@ -2,6 +2,7 @@
 open System.Text.RegularExpressions
 
 let supportedDelimiters = [ ','; '\n' ]
+let defaultDelimmiter = ";"
 let (|SingleDigitString|_|) input =
    let m = Regex.Match(input, @"^[0-9]+$")
    if (m.Success) then Some input else None
@@ -9,6 +10,20 @@ let (|SingleDigitString|_|) input =
 let (|TwoDigitString|_|) input =
    let m = Regex.Match(input, @"^([0-9]+(,|\n))+[0-9]+$")
    if (m.Success) then Some input else None
+
+let (|AdvancedString|_|) input =
+   let delimiters = [ ';'; ',' ]
+   let m = Regex.Match(input, @"^//(?<delim>.+)\n([0-9]+\k<delim>)+[0-9]+$")
+   if (m.Success) then Some(input.Split '\n') else None
+
+let getNumbersPart (originalString : String) =
+    originalString.Split('\n').[1]
+
+let getDelimitersPart (splitString : String []) =
+    if splitString.Length > 1 then
+        splitString.[0].Split([| "//" |], StringSplitOptions.RemoveEmptyEntries).[0]
+    else
+        defaultDelimmiter
 
 let rec add s =
     match s with
@@ -18,7 +33,11 @@ let rec add s =
         int x
     | TwoDigitString x ->
         x.Split([| ','; '\n' |]) |> Seq.map int |> Seq.sum
+    | AdvancedString x ->
+        let nums = getNumbersPart s
+        let delims = getDelimitersPart x
+        nums.Split([| delims |], StringSplitOptions.RemoveEmptyEntries) |> Seq.map int |> Seq.sum
     | _ ->
         failwith "Invalid Input"
-
-printfn "%i" (add "1\n2\n3,1000")
+        
+printfn "%i" (add "//,,\n12,,3,,1000")
